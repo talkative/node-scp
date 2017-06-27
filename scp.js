70d1c80f843ecae487bb455b10a88e8176b3b060
Martin Lundin <martin@talkative.se>
2017-06-27 14:37:55 +0200

    strict

--- scp.js
+++ scp.js
@@ -9,14 +9,15 @@ var scp = module.exports = {};
 /*
  * Transfer a file to a remote host
  */
+ //-o StrictHostKeyChecking=no
+ //-o CheckHostIP=no
 scp.send = function (options, cb) {
   var command = [
     'scp',
-    (options.keyfile == undefined ? '' : '-o CheckHostIP=no -i ' + options.keyfile),
+    (options.keyfile == undefined ? '' : '-o StrictHostKeyChecking=no -i ' + options.keyfile),
     '-r',
     '-P',
     (options.port == undefined ? '22' : options.port),
-    '-o "ControlMaster no"', //callback is not fired if ssh sessions are shared
     options.file,
     (options.user == undefined ? '' : options.user+'@') + options.host + ':' + options.path,
   ];
